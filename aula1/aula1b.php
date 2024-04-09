4f96cab0ca9b0cf867e262e202b63fa59b2230c7
Thiago Santos <131814290+Thiagosantos758@users.noreply.github.com>
2024-04-09 09:33:28 -0300

    Add files via upload

---
+++ aula1/aula1b.php
@@ -0,0 +1,13 @@
+<?php
+ $name = "Thiago";
+for ($a = 1; $a < mb_strlen($name); $a++){
+    $charAt = mb_strlen($a);
+    if($charAt == 'a' || $charAt == 'e' || $charAt == 'i' || $charAt == 'o' || $charAt == 'u'){
+        $vogais += 1;
+        print($qntvogais);
+    }
+}
+ echo("hello world!<br>");
+ echo("bartô<br>");
+ print("bartô print<br>");
+ ?>
